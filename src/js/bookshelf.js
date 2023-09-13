97489472e174a589dbc3dd7bc523068b35418147
praqo <142553687+praqo@users.noreply.github.com>
2023-09-12 21:33:08 -0700

    add bookSearch and fetchData functions

--- src/js/bookshelf.js
+++ src/js/bookshelf.js
@@ -1,3 +1,27 @@
 (function () {
-  console.log("hello world");
+  "use strict";
+
+  const bookSearchForm = document.querySelector(".js-book-search");
+  const bookSearchInput = document.querySelector("#bookSearch");
+  const searchResultsContainer = document.querySelector(".js-search-results");
+  const apiUrl = "http://openlibrary.org/search.json?q=";
+
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function searchBooks(e) {
+    e.preventDefault();
+
+    const searchValue = bookSearchInput.value.replace(/ /g, "+");
+    const data = fetchData(apiUrl + searchValue);
+  }
+
+  bookSearchForm.addEventListener("submit", searchBooks);
 })();

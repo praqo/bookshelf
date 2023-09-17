d265f3b730ccf73ed12dbfe9105ad4ea6befd5f6
praqo <142553687+praqo@users.noreply.github.com>
2023-09-16 21:33:08 -0700

    add userDataFunctions to list of js files to concatenate

--- src/js/results.js
+++ src/js/results.js
@@ -2,13 +2,22 @@ const results = (function () {
   if (document.querySelector(".js-search-results")) {
     const searchResultsContainer = document.querySelector(".js-search-results");
 
+    function emitAddBook(e) {
+      console.log(e.currentTarget.dataset.bookid);
+      events.emit("addBook", e.currentTarget.dataset.bookid);
+    }
+
     function populateData(data) {
       let htmlToAppend;
       data.docs.forEach((item) => {
-        htmlToAppend += `<h3 class='book-title'>${item.title}</h3>`;
+        htmlToAppend += `<h3 class='book-title' data-bookid=${item.key}>${item.title}</h3>`;
       });
 
       searchResultsContainer.innerHTML = htmlToAppend;
+
+      document
+        .querySelectorAll(".book-title")
+        .forEach((item) => item.addEventListener("click", emitAddBook));
     }
 
     events.on("searchDataChange", populateData);
